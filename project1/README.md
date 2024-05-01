bbb223443552084c771b69765a89313c4ba18e9d
Nupur <solnupur999.com>
2024-05-01 12:27:22 +0530

    first

---
+++ project1/README.md
@@ -0,0 +1,1 @@
+video: https://drive.google.com/file/d/1wG7QmtfWKHH-zbv4G0YUWOWGFoety00p/view?usp=sharing
---
+++ project1/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Component/Header";
+import Contain from "./Component/Contain";
+import Footer from "./Component/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Contain />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
