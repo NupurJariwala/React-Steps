a71566ea706ee9d4e67a8d6280ede8b3c4c2a7b4
Nupur <solnupur999.com>
2024-05-21 23:25:33 +0530

    first

--- IntegrateBootstrap/src/Bootstrap.jsx
+++ IntegrateBootstrap/src/Bootstrap.jsx
@@ -11,7 +11,7 @@ function Bootstrap() {
     <div>
       <Container>
         <Row className="text-left m-5 ">
-          <Col xs={6}>
+          <Col xs={6} md={6} lg={6} sm={6}>
             <h1>Hi,Hi,I'm Nupur Jariwala</h1>
             <br />
             <h5 className="text-secondary">
@@ -33,7 +33,7 @@ function Bootstrap() {
             <RiInstagramFill className="text-secondary m-3" />
             <BiSupport className="text-secondary m-2" />
           </Col>
-          <Col xs={6}>
+          <Col xs={6} md={6} lg={6} sm={6}>
             <img
               src="https://static.vecteezy.com/system/resources/previews/004/773/704/original/a-girl-s-face-with-a-beautiful-smile-a-female-avatar-for-a-website-and-social-network-vector.jpg"
               width="300px"
