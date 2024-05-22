f2098b264611e9a5a95ec91f9a4ad36afc70f189
Nupur <solnupur999.com>
2024-05-22 16:47:50 +0530

    first

--- EventHandler/src/Component/Post.jsx
+++ EventHandler/src/Component/Post.jsx
@@ -1,8 +1,9 @@
-export default function Post({ title, body }) {
+const Post = ({ title, body }) => {
   return (
     <div id="post">
       <h1>{title}</h1>
       <h3>{body}</h3>
     </div>
   );
-}
+};
+export default Post;
